0a3d118cacc5bc2d415a0a86e6e46eda6d8428ca
RafaelTomazGraciano <rafaeltomaz2005@gmail.com>
2025-08-15 17:28:12 -0300

    game completed and README

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Jumping_Mario
-A simple endless jumping game featuring Mario, inspired by the Google Chrome dinosaur game
+
+# Jumping Mario
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE)
+[![Made with HTML5](https://img.shields.io/badge/HTML5-%23E34F26.svg?&logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+[![Made with CSS3](https://img.shields.io/badge/CSS3-%231572B6.svg?&logo=css3&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/CSS)
+[![Made with JavaScript](https://img.shields.io/badge/JavaScript-%23F7DF1E.svg?&logo=javascript&logoColor=black)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+---
+
+## About
+
+Jumping Mario is a simple endless runner game inspired by the Google Chrome dinosaur game. Control Mario and jump over pipes to score as many points as possible. The game gets faster as your score increases!
+
+## Features
+- Responsive design
+- Start/Restart button
+- Score counter
+- Increasing difficulty (pipe speed)
+- Keyboard and mouse controls
+- Game over screen
+
+## How to Play
+- Press the **Start Game** button or hit the **Spacebar** to begin.
+- Use the **Spacebar** to make Mario jump over the pipes.
+- The game ends when Mario collides with a pipe.
+- Your score increases the longer you survive.
+- After a game over, press **Start Game** or **Spacebar** to play again.
+
+## Controls
+- **Spacebar**: Jump / Start Game
+- **Mouse Click**: Start Game
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+
+## Screenshots
+
+![In Game](./images/screen_in_game.png)
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
--- script.js
+++ script.js
@@ -1,34 +1,85 @@
+
 const mario = document.querySelector('.mario');
-const pipe = document.querySelector(".pipe")
+const pipe = document.querySelector('.pipe');
+const button = document.getElementById('start-button');
+const scoreDisplay = document.getElementById('score');
+let score = 0;
+let gameStarted = false;
+let loop = null;
+let scoreInterval = null;
+
+pipe.style.animation = 'none';
+pipe.style.display = 'none';
+
+function setPipeSpeed(currentScore = 0) {
+    let base = 3;
+    let min = 1.2;
+    let speed = Math.max(base - currentScore * 0.005, min);
+    pipe.style.animation = `pipe-animation ${speed}s linear infinite`;
+}
+
+function startGame() {
+    gameStarted = true;
+    pipe.style.left = '';
+    button.style.display = 'none';
+    score = 0;
+    scoreDisplay.textContent = score;
+    mario.src = './images/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '0px';
+    mario.style.bottom = '0px';
+    mario.style.animation = '';
+
+    pipe.style.display = 'block';
+    setPipeSpeed(score);
+
+    scoreInterval = setInterval(() => {
+        score++;
+        scoreDisplay.textContent = score;
+        setPipeSpeed(score);
+    }, 1000);
+
+    loop = setInterval(() => {
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = './images/game-over.png';
+            mario.style.width = '80px';
+            mario.style.marginLeft = '60px';
+            
+            button.style.display = 'block';
+            gameStarted = false;
+            clearInterval(scoreInterval);
+            clearInterval(loop);
+        }
+    }, 10);
+}
 
 const jump = () => {
+    if (!gameStarted) return;
     mario.classList.add('jump');
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
 }
 
-const loop = setInterval(() =>{
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100){
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+button.addEventListener('click', startGame);
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = './images/game-over.png';
-        mario.style.width = '80px';
-        mario.style.marginLeft = '60px';
-
-        clearInterval(loop);
-    }
-}, 10)
 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
-        jump();
+        if (!gameStarted) {
+            startGame();
+        } else {
+            jump();
+        }
     }
 });
